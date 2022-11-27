effb848e836f02541c7a9e2955b47729f306baeb
Leandro Cunha <leandroscunha@gmail.com>
2022-11-27 14:00:57 +0100

    fix: cleanup imports to import correct CoursesComponents

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, Route } from '@angular/router';
 import { CoursesComponent } from '../features/courses/courses.component';
-import { LoginComponent } from '../features/login/login.component';
-import { RegistrationComponent } from '../features/registration/registration.component';
+//import { LoginComponent } from '../features/login/login.component';
+//import { RegistrationComponent } from '../features/registration/registration.component';
 
 const appRoutes: Route[] = [
-  {
+/*  {
     path: 'login',
     loadChildren: () =>
       import('../features/login/login.module').then((m) => m.LoginModule),
@@ -21,7 +21,7 @@ const appRoutes: Route[] = [
     path: 'courses',
     loadChildren: () =>
       import('../features/courses/courses.module').then((m) => m.CoursesModule),
-  },
+  },*/
   {
     path: '',
     component: CoursesComponent,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './features/login/login.component';
-import { RegistrationComponent } from './features/registration/registration.component';
+//import { LoginComponent } from './features/login/login.component';
+//import { RegistrationComponent } from './features/registration/registration.component';
 import { CoursesComponent } from './features/courses/courses.component';
-import { CourseComponent } from './features/course/course.component';
-import { CoursesModule } from './features/courses/courses.module';
-import { CourseCardComponent } from './features/course-card/course-card.component';
+//import { CoursesModule } from './features/courses/courses.module';
+/*import { CourseCardComponent } from './features/course-card/course-card.component';
 import { CourseListComponent } from './features/course-list/course-list.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CourseEditCreateComponent } from './features/course-edit-create/course-edit-create.component';
-import { SharedModule } from './shared/shared.module';
+import { SharedModule } from './shared/shared.module';*/
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegistrationComponent,
+    // LoginComponent,
+    // RegistrationComponent,
     CoursesComponent,
-    CourseComponent,
-    CourseCardComponent,
-    CourseListComponent,
-    CourseEditCreateComponent,
+    // CourseCardComponent,
+    // CourseListComponent,
+    // CourseEditCreateComponent,
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
-    CoursesModule,
-    FontAwesomeModule,
-    FormsModule,
-    ReactiveFormsModule,
-    SharedModule,
+ //   CoursesModule,
+  //  FontAwesomeModule,
+    // FormsModule,
+    // ReactiveFormsModule,
+//    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
