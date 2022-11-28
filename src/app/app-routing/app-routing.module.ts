62d4c916a4ba8b4c8fa8aa7d6f89bd3845a38710
Leandro Cunha <leandroscunha@gmail.com>
2022-11-28 13:58:27 +0100

    feat: update root router to show LoginComponent

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, Route } from '@angular/router';
 import { CoursesComponent } from '../features/courses/courses.component';
+import { LoginComponent } from '../features/login/login.component';
 //import { LoginComponent } from '../features/login/login.component';
 //import { RegistrationComponent } from '../features/registration/registration.component';
 
 const appRoutes: Route[] = [
-  /*  {
-    path: 'login',
-    loadChildren: () =>
-      import('../features/login/login.module').then((m) => m.LoginModule),
-  },
   {
+    path: '',
+    component: LoginComponent,
+  },
+  /*{
     path: 'registration',
     loadChildren: () =>
       import('../features/registration/registration.module').then(
@@ -23,7 +23,7 @@ const appRoutes: Route[] = [
       import('../features/courses/courses.module').then((m) => m.CoursesModule),
   },*/
   {
-    path: '',
+    path: 'courses',
     loadChildren: () =>
       import('../features/courses/courses.module').then((m) => m.CoursesModule),
   },
--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, SharedModule],
 })
-export class LoginModule { }
+export class LoginModule {}
